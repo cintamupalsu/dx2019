b24c02ed13f1135bfc42992a67a50836d1c8239e
a_maulana <cintamupalsu@hotmail.com>
2019-11-11 15:43:30 +0900

    VR 02

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,9 +2,34 @@ class ReportsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    # IBM Watson Initialization
+    apikey = Rails.application.secrets.watson_visual_recognition_api_key
+    authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+      apikey: apikey
+    )
+    visual_recognition = IBMWatson::VisualRecognitionV3.new(
+      version: "2018-03-19",
+      authenticator: authenticator
+    )
+    # End Initialization
+
     vessel = Vessel.find(report_params['vessel_id'])
     @report = vessel.reports.build(report_params)
     @report.image.attach(params[:report][:image])
+
+    # Image file processing
+    if report_params['image'] != nil
+      file = report_params['image']
+      File.open(file.path) do |images_file|
+        classes = visual_recognition.classify(
+          images_file: images_file,
+          threshold: "0.6",
+          classifier_ids: ["default"]
+        )
+        @report.content = classes.result["images"][0]["classifiers"][0]["classes"]
+      end
+    end
+
     if @report.save
       ActionCable.server.broadcast 'room_channel', content: @report.content, picture: rails_blob_path(@report.image, disposition: "attachment", only_path: true)
       flash[:success] = "Report created!"
